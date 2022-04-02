1103f6df6adc05e4fb3529f706a2ac00a29cdeec
Victor Aderibigbe <victoraderibigbe03@gmail.com>
2022-04-02 20:52:49 +0100

    Update Staker.sol

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -12,6 +12,54 @@ contract Staker {
       exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
   }
 
+
+    mapping(address=>uint256) public balances;
+    uint256 public constant threshhold = 1 ether;
+    uint256 public deadline = block.timestamp + 90 seconds;
+    bool executed = false;
+    bool openForWithdraw = false;
+    event Stake(address staker, uint256 value);
+
+
+  function stake() public payable{
+        balances[msg.sender] = msg.value;
+      emit Stake(msg.sender, msg.value);
+  }
+
+    function execute()public returns(bool){
+        uint256 time = timeLeft();
+        if (time>0){
+            return false;
+            }
+
+        if (executed){
+            return false;
+        }
+        if (address(this).balance >=threshhold){
+            exampleExternalContract.complete{value: address(this).balance}();
+            executed = true;
+            return true;
+            }
+        openForWithdraw = true;
+        executed = true;
+        return true;
+    }
+
+    function withdraw()public returns(bool){
+        if(openForWithdraw) return true;
+        return false;
+    }
+
+  function timeLeft()public view returns(uint256) {
+      if (block.timestamp >= deadline) return 0;
+      uint256 left = deadline - block.timestamp;
+      return left;
+  }
+    receive() external payable{
+    stake();
+    }
+
+
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
 
